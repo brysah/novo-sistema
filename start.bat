12a9ffe82b301d85819d05ac67656a793d473aa8
brysah <93356874+brysah@users.noreply.github.com>
2025-10-26 05:41:52 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,10 +2,11 @@
 setlocal
 
 REM ===============================
-REM Desativa Quick Edit Mode
+REM Desativa Quick Edit Mode (solução real)
 REM ===============================
 if "%1" neq "noquickedit" (
-    start "JointMotion System" cmd /k call "%~f0" noquickedit
+    powershell -Command "Add-Type -Name disableEdit -Namespace tmp -MemberDefinition '[DllImport(\"kernel32.dll\")]public static extern IntPtr GetStdHandle(int nStdHandle);[DllImport(\"kernel32.dll\")]public static extern bool GetConsoleMode(IntPtr hConsoleHandle,out uint lpMode);[DllImport(\"kernel32.dll\")]public static extern bool SetConsoleMode(IntPtr hConsoleHandle,uint dwMode);' ; $h = [tmp.disableEdit]::GetStdHandle(-10); $m = 0; [tmp.disableEdit]::GetConsoleMode($h,[ref]$m); [tmp.disableEdit]::SetConsoleMode($h,$m -band -bnot 64)" >nul 2>&1
+    call "%~f0" noquickedit
     exit /b
 )
 
